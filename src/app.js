a8c7f553e69b5dcf73ed40a28f658478ded9dc3f
Your Name <you@example.com>
2025-04-27 14:41:57 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 const express=require("express");
 
 const app=express();
-app.get("/user",(req,res)=>{
+app.get("/u(s)?er",(req,res)=>{
     res.send({firstname:"vamshi",lastname:"yenaganti"})
 });
 
-app.post("/user",(req,res)=>{
-    //data is saved to DB
-    res.send("data successfully saved");
-});
-app.delete("/user",(req,res)=>{
-    //data is deleted from db
-    res.send("data is deleted from db");
-});
+
 
 
 app.listen(3000,()=>{console.log("server is successfully listening on port 3000")});
